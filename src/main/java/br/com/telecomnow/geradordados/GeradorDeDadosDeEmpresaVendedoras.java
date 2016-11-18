5a7ab498658db5f841b21cd0aa32128e01c8e011
Sandrini <sandrinibrum@gmail.com>
2016-11-18 19:09:45 -0200

    Ajustando imagens

--- src/main/java/br/com/telecomnow/geradordados/GeradorDeDadosDeEmpresaVendedoras.java
+++ src/main/java/br/com/telecomnow/geradordados/GeradorDeDadosDeEmpresaVendedoras.java
@@ -13,11 +13,11 @@ public class GeradorDeDadosDeEmpresaVendedoras {
 
     public Collection<EmpresaVendedora> gerarDados() {
         Collection<EmpresaVendedora> empresas = new ArrayList<>();
-        empresas.add(new EmpresaVendedora("Google", PLATINUM, "http://www.google.com", "img/logo-empresa.png"));
-        empresas.add(new EmpresaVendedora("Apple", GOLD, "http://www.apple.com", "img/logo-empresa.png"));
-        empresas.add(new EmpresaVendedora("HP", GOLD, "http://www.hp.com", "img/logo-empresa.png"));
-        empresas.add(new EmpresaVendedora("DELL", SILVER, "http://www.dell.com", "img/logo-empresa.png"));
-        empresas.add(new EmpresaVendedora("NeoGrid", SILVER, "http://www.neogrid.com", "img/logo-empresa.png"));
+        empresas.add(new EmpresaVendedora("Google", PLATINUM, "http://www.google.com", "img/logo-empresa-1.png"));
+        empresas.add(new EmpresaVendedora("Apple", GOLD, "http://www.apple.com", "img/logo-empresa-2.png"));
+        empresas.add(new EmpresaVendedora("HP", GOLD, "http://www.hp.com", "img/logo-empresa-3.png"));
+        empresas.add(new EmpresaVendedora("DELL", SILVER, "http://www.dell.com", "img/logo-empresa-4.png"));
+        empresas.add(new EmpresaVendedora("NeoGrid", SILVER, "http://www.neogrid.com", "img/logo-empresa-5.png"));
         return empresas;
     }
 }
