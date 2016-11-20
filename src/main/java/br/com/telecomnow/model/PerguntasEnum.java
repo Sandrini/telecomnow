6ebb220da0e072f9e0e3f5ee422a94d194ddb084
Sandrini <sandrinibrum@gmail.com>
2016-11-20 21:58:26 -0200

    perguntas descricoes

--- src/main/java/br/com/telecomnow/model/PerguntasEnum.java
+++ src/main/java/br/com/telecomnow/model/PerguntasEnum.java
@@ -4,22 +4,22 @@ public enum PerguntasEnum {
 
 	UNIDADE("Sua empresa possui, além da matriz, mais unidades (filiais)?",
 			"TELEATENDIMENTO",
-			"<explicar aqui a sobre o componente>"),
+			"PABX é a central telefônica analógica propriamente dita. É o conceito de ramal ramal. Tarifador Centralizado computa os custos de ligação de todos os ramais. Consigo saber quanto o ramal de determinado funcionário gastou em ligações, no período de um mês. No projeto acima teremos uma Matriz que se comunicará com as demais Filiais, através do STFC (Serviço Telefônico Fixo Comutado - destinado a comunicação entre pontos fixos)."),
 	TELEATENDIMENTO("Há necessidade de inserir solução de teleatendimento (para call center ou sac), em sua empresa",
 			"TELEINTEGRACAO",
-			"<explicar aqui a sobre o componente>"),
+			"Atendedor Digital é a Ura Simplificada, onde temos uma gravação com a seguinte mensagem: Digite 1 para informações da faculdade, 2  secretaria, 3 financeiro, etc). URA é quando o atendimento efetuado assemelha-se ao da NET. "),
 	TELEINTEGRACAO("Deseja integração da solução de teleatendimento com seu sistema de gestão?",
 			"CHAT",
-			"<explicar aqui a sobre o componente>"),
+			"Tele Integração é integrar a Solução de Tele antendimento com outros softwares que o cliente utiliza na empresa."),
 	CHAT("Alem de atendimento via voz, sua demanda necessita de atendimento via CHAT?",
 			"GRAVACAO",
-			"<explicar aqui a sobre o componente>"),
+			"Contact Center é um centro de ligação mais completo, onde é possível atender o cliente via chat, email e via voz."),
 	GRAVACAO("Deseja realizar gravação dos atendimentos?",
 			"CELULAR",
-			"<explicar aqui a sobre o componente>"),
+			"A gravação da ligação se dá quando o cliente necessita por questões de segurança ou por questões de qualidade."),
 	CELULAR("Deseja incluir solução de interface de celular?",
 			null,
-			"<explicar aqui a sobre o componente>"),
+			"Interface Celular é um hardware, junto à Central Telefônica, que possui vários chips de várias operadoras de telefonia e esse hardware faz consulta portabilidade. Quando um funcionário da empresa efetua uma ligação para um celular, esse equipamento consulta qual a operadora de destino da ligação, após identificada o hardware utiliza o chip correspondente a mesma operadora, efetuando dessa forma uma ligação a custo zero para a empresa."),
 	;
 	
 	private final String mensagem;
